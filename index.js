5378702321c55ec9c8b7ae79782cde247a6d72f0
Héctor Cortez <updown.input@gmail.com>
2023-10-12 09:19:11 -0300

    feat: load the transactions in the DOM

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const form = document.querySelector(".add"); // selecting the form to add transaction
+const incomeList = document.querySelector("ul.income-list");
+const expenseList = document.querySelector("ul.expense-list");
 
 let transactions = localStorage.getItem("transactions") !== null ?
     JSON.parse(localStorage.getItem("transactions")) : []
@@ -15,6 +17,25 @@ function generateFormatedtLocalDateTime() {
     return `${date} at ${hours}:${minutes}:${seconds} ${amOrPm}`;
 }
 
+function generateHtmlTemplate(id, source, amount, time) {
+    return `<li data-id="${id}">
+                <p>
+                    <span>${source}</span>
+                    <span id="time">${time}</span>
+                </p>
+                <span>$${Math.abs(amount)}</span>
+                <i class="bi bi-trash delete"></i>
+            </li>`;
+}
+
+function addTransactionDOM(id, source, amount, time) {
+    amount > 0 ?
+        incomeList.innerHTML += generateHtmlTemplate(id, source, amount, time)
+        :
+        expenseList.innerHTML += generateHtmlTemplate(id, source, amount, time)
+
+}
+
 function generateID() {
     return Math.round(Math.random() * 1000000);
 }
@@ -28,7 +49,23 @@ function generateAndAddTransactionToList(source, amount) {
     };
 
     transactions.push(transaction);
+    return transaction;
 }
+
+/**
+ * Take each transaction and show it in income or expenses.
+ */
+function loadTransactionsToDOM() {
+    transactions.forEach(transaction =>
+        transaction.amount > 0 ?
+            incomeList.innerHTML += generateHtmlTemplate(transaction.id, transaction.source, transaction.amount, transaction.time)
+            :
+            expenseList.innerHTML += generateHtmlTemplate(transaction.id, transaction.source, transaction.amount, transaction.time)
+    );
+    console.log(transactions)
+}
+
+loadTransactionsToDOM();
 
 /**
  * Creates a transaction with the values
@@ -37,7 +74,8 @@ function generateAndAddTransactionToList(source, amount) {
  */
 form.addEventListener("submit", event => {
     event.preventDefault();
-    generateAndAddTransactionToList(form.source.value, form.amount.value);
+    const transaction = generateAndAddTransactionToList(form.source.value, form.amount.value);
     localStorage.setItem("transactions", JSON.stringify(transactions));
+    addTransactionDOM(transaction.id, transaction.source, transaction.amount, transaction.time);
     form.reset();
 });
