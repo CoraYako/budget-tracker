c13769bb24398261c31713ea79d470cf2dba5373
Héctor Cortez <updown.input@gmail.com>
2023-10-13 08:07:28 -0300

    values of statistics updated

--- index.js
+++ index.js
@@ -2,8 +2,33 @@ const form = document.querySelector(".add"); // selecting the form to add transa
 const incomeList = document.querySelector("ul.income-list");
 const expenseList = document.querySelector("ul.expense-list");
 
+const income = document.getElementById("income")
+const expense = document.getElementById("expense");
+const balance = document.getElementById("balance");
+
+const errorMessage = document.querySelector("form p.message"); // <p> element to show error messages if the inputs are invalid
+
 let transactions = localStorage.getItem("transactions") !== null ?
     JSON.parse(localStorage.getItem("transactions")) : []
+
+/**
+ * Updates the values for balance, income and expense
+ * in the statistics field.
+ */
+function updateStatistics() {
+    const updatedIncome = transactions
+        .filter(transaction => transaction.amount > 0)
+        .reduce((total, transaction) => total += transaction.amount, 0);
+
+    const updatedExpense = transactions
+        .filter(transaction => transaction.amount < 0)
+        .reduce((total, transaction) => total += Math.abs(transaction.amount), 0);
+
+    balance.textContent = updatedIncome - updatedExpense;
+    income.textContent = updatedIncome;
+    expense.textContent = updatedExpense;
+}
+updateStatistics();
 
 function generateFormatedtLocalDateTime() {
     const time = new Date();
@@ -67,34 +92,53 @@ function loadTransactionsToDOM() {
             :
             expenseList.innerHTML += generateHtmlTemplate(transaction.id, transaction.source, transaction.amount, transaction.time)
     );
-    console.log(transactions)
 }
-
 loadTransactionsToDOM();
+
+// --------------------------------------------------------------------
 
 /**
  * Creates a transaction with the values
  * passed through the source and amount input
- * in the application form.
+ * in the application form and updates the
+ * statistics (balance, income, expense).
  */
 form.addEventListener("submit", event => {
     event.preventDefault();
-    const transaction = generateAndAddTransactionToList(form.source.value, form.amount.value);
+    if (form.source.value.trim() === "" || form.amount.value.trim() === "" || form.amount.value.trim() === "0") {
+        errorMessage.classList.remove("hide");
+        errorMessage.textContent = "Please, add proper values.";
+        errorMessage.classList.add("error-message");
+        return;
+    }
+    errorMessage.classList.add("hide");
+    const transaction = generateAndAddTransactionToList(form.source.value.trim(), Number.parseInt(form.amount.value));
     localStorage.setItem("transactions", JSON.stringify(transactions));
     addTransactionDOM(transaction.id, transaction.source, transaction.amount, transaction.time);
     form.reset();
+    updateStatistics();
 });
 
+/**
+ * Remove the selected transaction in the
+ * income list and updates the statistics
+ */
 incomeList.addEventListener("click", event => {
     if (event.target.classList.contains("delete")) {
         event.target.parentElement.remove();
         deleteTransaction(Number.parseInt(event.target.parentElement.dataset.id));
     }
+    updateStatistics();
 });
 
+/**
+ * Remove the selected transaction in the
+ * expense list and updates the statistics
+ */
 expenseList.addEventListener("click", event => {
     if (event.target.classList.contains("delete")) {
         event.target.parentElement.remove();
         deleteTransaction(Number.parseInt(event.target.parentElement.dataset.id));
     }
+    updateStatistics();
 });
